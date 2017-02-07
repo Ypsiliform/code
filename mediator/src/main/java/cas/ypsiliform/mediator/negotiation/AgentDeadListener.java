239d00b14824eb9ee890fe78ebbef4c679e74735
Michael Müller <madmike@outlook.de>
2017-02-07 12:05:51 +0100

    Add agent proxy for abstraction of communication handling

---
+++ mediator/src/main/java/cas/ypsiliform/mediator/negotiation/AgentDeadListener.java
@@ -0,0 +1,5 @@
+package cas.ypsiliform.mediator.negotiation;
+
+public interface AgentDeadListener {
+	public void OnAgentDead(AgentProxy agent);
+}
---
+++ mediator/src/main/java/cas/ypsiliform/mediator/negotiation/AgentProxy.java
@@ -0,0 +1,24 @@
+package cas.ypsiliform.mediator.negotiation;
+
+import java.util.ArrayList;
+
+import cas.ypsiliform.mediator.async.Thenable;
+import cas.ypsiliform.messages.AgentResponse;
+
+public class AgentProxy {
+	private ArrayList<AgentDeadListener> listeners = new ArrayList<AgentDeadListener>();
+	
+	public void addAgentDeadListener(AgentDeadListener listener) {
+		listeners.add(listener);
+	}
+	
+	public void removeAgentDeadListener(AgentDeadListener listener) {
+		listeners.remove(listener);
+	}
+	
+	
+	public Thenable<AgentResponse> sendSolutionProposal(SolutionProposal solution) {
+		// TODO implementation missing
+		throw new UnsupportedOperationException();
+	}
+}
