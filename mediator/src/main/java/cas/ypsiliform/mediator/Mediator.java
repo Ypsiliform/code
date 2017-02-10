234afb328a8ba9bc55ff6fb1b6402daa84b48ef5
Michael Müller <madmike@outlook.de>
2017-02-10 12:11:24 +0100

    Refactor Mediator to separate recursion logic and actual messaging

--- mediator/src/main/java/cas/ypsiliform/mediator/Mediator.java
+++ mediator/src/main/java/cas/ypsiliform/mediator/Mediator.java
@@ -2,16 +2,17 @@ package cas.ypsiliform.mediator;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 
 import cas.ypsiliform.Constants;
-import cas.ypsiliform.mediator.async.Function;
 import cas.ypsiliform.mediator.async.Thenable;
 import cas.ypsiliform.mediator.negotiation.AgentProxy;
+import cas.ypsiliform.mediator.negotiation.AgentVisitor;
+import cas.ypsiliform.mediator.negotiation.AgentVisitor.VisitorResult;
 import cas.ypsiliform.mediator.negotiation.SolutionProposal;
 import cas.ypsiliform.messages.AgentResponse;
 import cas.ypsiliform.messages.EndNegotiation;
@@ -26,8 +27,9 @@ import cas.ypsiliform.messages.Solution;
 public class Mediator implements Runnable {
 	private static Logger log = Logger.getLogger(Mediator.class.getSimpleName());
 
-	private final Map<Integer, AgentProxy> agents;
+	private final AgentVisitor visitor;
 	private final Integer[] primaryDemands;
+	private final int numberOfProposals;
 
 	/**
 	 * Mediator takes a list of agents that take part in the negotiation and the
@@ -50,20 +52,23 @@ public class Mediator implements Runnable {
 			throw new IllegalArgumentException();
 		}
 
-		this.agents = agents;
+		this.visitor = new AgentVisitor(agents, 1);
 		this.primaryDemands = primaryDemands;
+		this.numberOfProposals = agents.size() - 1;
 
 		registerAgentDeadListener(agents.values());
+
+		log.info("Mediator initialized with " + agents.size() + " agents");
 	}
 
 	@Override
 	public void run() {
-		log.info("Starting new negotiation with " + agents.size() + " agents");
-
 		List<SolutionProposal> solutions = new ArrayList<SolutionProposal>();
 		solutions.add(new SolutionProposal(Constants.Encoding.NUMBER_OF_PERIODS));
 
-		for (int i = 0; i < Constants.Negotiation.NUMBER_OF_ROUNDS; i++) {
+		AtomicBoolean stop = new AtomicBoolean(false);
+
+		for (int i = 0; i < Constants.Negotiation.NUMBER_OF_ROUNDS && !stop.get(); i++) {
 			log.fine("Entering round " + i);
 
 			SolutionProposal[] nextGen = generateOffspring(getNumberOfProposals(),
@@ -76,43 +81,33 @@ public class Mediator implements Runnable {
 				proposals.put(j + 1, new Proposal(nextGen[j], primaryDemands));
 			}
 
-			recursiveNegotiation(agents.get(1), proposals).then((Function<Map<Integer, Integer>, Thenable>) votes -> {
-				int[] histogram = new int[getNumberOfProposals()];
+			visitor.<Map<Integer, Proposal>, AgentResponse, Map<Integer, Integer>>visit(proposals,
+					this::collectResponses, this::reduceToFrequencyHistogram).then(histogram -> {
+						int maxVotes = histogram.values().stream().max(Integer::compare).orElse(0);
 
-				votes.forEach((agentId, solution) -> {
-					// assuming solutions are numbered 1 - n
-					histogram[solution - 1]++;
-				});
+						solutions.clear();
 
-				int maxVotes = 0;
-				for (int j = 0; j < histogram.length; j++) {
-					maxVotes = Math.max(maxVotes, histogram[j]);
-				}
+						histogram.forEach((key, votes) -> {
+							if (votes >= maxVotes)
+								solutions.add(proposals.get(key).solution);
+						});
 
-				solutions.clear();
+						return Thenable.resolved(null);
+					}).wait(Constants.Negotiation.TIMEOUT_PER_ROUND_MS, r -> {
+						log.warning("Negotiation round timed out");
+						stop.set(true);
+					});
 
-				for (int j = 0; j < histogram.length; j++) {
-					if (histogram[j] >= maxVotes)
-						solutions.add(nextGen[j]);
-				}
-
-				Thenable stop = new Thenable();
-				stop.resolve(null);
-				return stop;
-			}).wait(Constants.Negotiation.TIMEOUT_PER_ROUND_MS, r -> {
-				log.warning("Negotiation round timed out");
-				return;
-			});
 		}
 
 		SolutionProposal chosenSolution = solutions.get(0);
 		log.info("Chosen solution: " + chosenSolution.toString());
 
-		endNegotiationRecursive(agents.get(1), new Proposal(chosenSolution, primaryDemands))
+		visitor.visit(new Proposal(chosenSolution, primaryDemands), this::endNegotiation, null)
 				.wait(Constants.Negotiation.TIMEOUT_PER_ROUND_MS, r -> {
 					log.warning("Results communication timed out");
-					return;
 				});
+
 		log.info("Negotiation ended");
 	}
 
@@ -128,8 +123,7 @@ public class Mediator implements Runnable {
 	 * solution space faster.
 	 */
 	private int getNumberOfProposals() {
-		assert agents.size() > 2 : "Algorithm can only handle negotiations with more than two agents";
-		return agents.size() - 1;
+		return numberOfProposals;
 	}
 
 	/**
@@ -158,10 +152,10 @@ public class Mediator implements Runnable {
 	}
 
 	/**
-	 * Recursively traverse the supply chain and send the demands of higher
-	 * levels to the next levels. Uses asynchronous communication with agents
-	 * and can communicate with multiple agents on the same level at the same
-	 * time.
+	 * Step for the AgentVisitor to recursively traverse the supply chain and
+	 * send the demands of higher levels to the next levels. Uses asynchronous
+	 * communication with agents and can communicate with multiple agents on the
+	 * same level at the same time.
 	 * 
 	 * @param current
 	 *            Current agent to communicate with. Will propagate the current
@@ -170,59 +164,31 @@ public class Mediator implements Runnable {
 	 *            Solution proposals to communicate to the agent
 	 * @return A map of the agents' preferences (agent id => solution id)
 	 */
-	private Thenable<Map<Integer, Integer>> recursiveNegotiation(AgentProxy current, Map<Integer, Proposal> proposals) {
-		Thenable<Map<Integer, Integer>> result = new Thenable<Map<Integer, Integer>>();
-
-		// simple Integer is not allowed: "Local variable currentAgentPreference
-		// defined in an enclosing scope must be final or effectively final"
-		// stupid Java, fooled so easily
-		Integer[] currentAgentPreference = { 0 };
+	private Thenable<AgentVisitor.VisitorResult<Map<Integer, Proposal>, AgentResponse>> collectResponses(
+			AgentProxy current, Map<Integer, Proposal> proposals) {
+		Thenable<AgentVisitor.VisitorResult<Map<Integer, Proposal>, AgentResponse>> result = new Thenable<AgentVisitor.VisitorResult<Map<Integer, Proposal>, AgentResponse>>();
 
 		MediatorRequest request = getMediatorRequestMessage(current.getId(), proposals);
 
 		// send message to current agent asynchronously
 		current.sendSolutionProposals(request).then(response -> {
-			List<Thenable> nextIteration = new ArrayList<Thenable>();
-
 			Map<Integer, Integer[]> agentDemandVariants = readDependentDemands(response, request.getSolutions().size());
 
-			currentAgentPreference[0] = response.getSelection();
-
-			log.finer("Agent " + current.getId() + " prefers solution " + currentAgentPreference[0]);
+			int currentAgentPreference = response.getSelection();
+			log.finer("Agent " + current.getId() + " prefers solution " + currentAgentPreference);
 
 			// prepare recursion: Get full solution proposal (not only view for
 			// previous agent) and secondary demands
-			Map<Integer, Proposal> nextIterationProposals = new HashMap<Integer, Proposal>();
+			Map<Integer, Proposal> nextIterationProposals = new HashMap<Integer, Proposal>(proposals.size());
 
-			for (int j = 1; j <= agentDemandVariants.size(); j++) {
-				Integer[] demandVariant = agentDemandVariants.get(j);
+			agentDemandVariants.forEach((key, dependentDemand) -> {
+				assert dependentDemand.length == Constants.Encoding.NUMBER_OF_PERIODS : "Number of secondary demands should equal number of periods";
 
-				assert demandVariant.length == Constants.Encoding.NUMBER_OF_PERIODS : "Number of secondary demands should equal number of periods";
-
-				nextIterationProposals.put(j, new Proposal(proposals.get(j).solution, demandVariant));
-			}
-
-			// send solution + secondary demands of current agent to all
-			// children
-			current.getChildIds().forEach(childId -> {
-				log.finer("Asking agent " + childId);
-				nextIteration.add(recursiveNegotiation(agents.get(childId), nextIterationProposals));
+				nextIterationProposals.put(key, new Proposal(proposals.get(key).solution, dependentDemand));
 			});
 
-			Map<Integer, Integer> pref = new HashMap<Integer, Integer>();
-			pref.put(current.getId(), currentAgentPreference[0]);
-
-			if (nextIteration.isEmpty()) {
-				result.resolve(pref);
-			} else {
-				// collect votes from recursive calls
-				Thenable.whenAll(nextIteration).then(results -> {
-					for (int i = 0; i < results.length; i++) {
-						pref.putAll((Map<Integer, Integer>) results[i]);
-					}
-					result.resolve(pref);
-				});
-			}
+			result.resolve(new AgentVisitor.VisitorResult<Map<Integer, Proposal>, AgentResponse>(nextIterationProposals,
+					response));
 		});
 
 		return result;
@@ -239,8 +205,10 @@ public class Mediator implements Runnable {
 	 * @param chosenSolution
 	 *            The solution that is to be communicated to all agents
 	 */
-	private Thenable<Void> endNegotiationRecursive(AgentProxy current, Proposal chosenSolution) {
-		Thenable<Void> result = new Thenable<Void>();
+
+	private Thenable<AgentVisitor.VisitorResult<Proposal, Void>> endNegotiation(AgentProxy current,
+			Proposal chosenSolution) {
+		Thenable<AgentVisitor.VisitorResult<Proposal, Void>> result = new Thenable<AgentVisitor.VisitorResult<Proposal, Void>>();
 
 		// map view on bit string and primary secondary/secondary demands from
 		// previous agent into message
@@ -251,8 +219,6 @@ public class Mediator implements Runnable {
 
 		// send message to current agent asynchronously
 		current.sendSolutionProposals(request).then(response -> {
-			List<Thenable> runningMessages = new ArrayList<Thenable>();
-
 			Map<Integer, Integer[]> agentDemandVariants = readDependentDemands(response, 1);
 
 			// prepare recursion: Get full solution (not only view for previous
@@ -263,24 +229,34 @@ public class Mediator implements Runnable {
 
 			Proposal nextIterationSolution = new Proposal(chosenSolution.solution, demandVariant);
 
-			// send solution + secondary demands of current agent to all
-			// children
-			current.getChildIds().forEach(childId -> {
-				log.finer("Asking agent " + childId);
-				runningMessages.add(endNegotiationRecursive(agents.get(childId), nextIterationSolution));
-			});
-
 			// confirm solution to current agent
+			// fire and forget
 			EndNegotiation message = new EndNegotiation();
 			message.setSolution(request.getSolutions().get(1));
-			runningMessages.add(current.endNegotiation(message));
+			current.endNegotiation(message);
 
-			Thenable.whenAll(runningMessages).then(r -> {
-				result.resolve(null);
-			});
+			// send solution + secondary demands of current agent to all
+			// children
+			result.resolve(new VisitorResult<Mediator.Proposal, Void>(nextIterationSolution, null));
 		});
 
 		return result;
+	}
+
+	private Map<Integer, Integer> reduceToFrequencyHistogram(AgentResponse response, Object[] otherVotes) {
+		Map<Integer, Integer> histogram = new HashMap<Integer, Integer>(getNumberOfProposals());
+		histogram.put(response.getSelection(), 1);
+
+		if (otherVotes != null)
+			for (int j = 0; j < otherVotes.length; j++) {
+				((Map<Integer, Integer>) otherVotes[j]).forEach((key, count) -> {
+					histogram.merge(key, count, (prev, next) -> {
+						return prev + next;
+					});
+				});
+			}
+
+		return histogram;
 	}
 
 	private MediatorRequest getMediatorRequestMessage(int agentId, Map<Integer, Proposal> proposals) {
---
+++ mediator/src/main/java/cas/ypsiliform/mediator/negotiation/AgentVisitor.java
@@ -0,0 +1,81 @@
+package cas.ypsiliform.mediator.negotiation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import cas.ypsiliform.mediator.Mediator;
+import cas.ypsiliform.mediator.async.Action;
+import cas.ypsiliform.mediator.async.Function2;
+import cas.ypsiliform.mediator.async.Thenable;
+
+public class AgentVisitor {
+	private static Logger log = Logger.getLogger(Mediator.class.getName());
+
+	private final Map<Integer, AgentProxy> agents;
+	private final int startAgentID;
+
+	public AgentVisitor(Map<Integer, AgentProxy> agents, int startAgentID) {
+		if (!agents.containsKey(startAgentID))
+			throw new IllegalArgumentException("startAgentID");
+
+		this.agents = agents;
+		this.startAgentID = startAgentID;
+	}
+
+	public <Parameter, IterationResult, Result> Thenable<Result> visit(Parameter initialParameter,
+			Function2<AgentProxy, Parameter, Thenable<VisitorResult<Parameter, IterationResult>>> activity,
+			Function2<IterationResult, Object[], Result> reduction) {
+		return visitRecursive(agents.get(startAgentID), initialParameter, activity, reduction);
+	}
+
+	private <Parameter, IterationResult, Result> Thenable<Result> visitRecursive(AgentProxy current, Parameter param,
+			Function2<AgentProxy, Parameter, Thenable<VisitorResult<Parameter, IterationResult>>> activity,
+			Function2<IterationResult, Object[], Result> reduction) {
+
+		Thenable<Result> result = new Thenable<Result>();
+
+		List<Thenable> nextIteration = new ArrayList<Thenable>();
+
+		activity.perform(current, param).then(currentResult -> {
+			current.getChildIds().forEach(childId -> {
+				log.finer("Asking agent " + childId);
+
+				assert agents.containsKey(childId) : "Invalid reference to child agent";
+				AgentProxy child = agents.get(childId);
+
+				nextIteration.add(visitRecursive(child, currentResult.NextIterationParameter, activity, reduction));
+			});
+
+			if (nextIteration.size() == 0) {
+				if (reduction == null)
+					result.resolve(null);
+				else
+					result.resolve(reduction.perform(currentResult.CurrentIterationResult, null));
+			} else {
+				Thenable.whenAll(nextIteration).then((Action<Object[]>) nextIterationResult -> {
+					Result r = null;
+
+					if (reduction != null) {
+						r = reduction.perform(currentResult.CurrentIterationResult, nextIterationResult);
+					}
+
+					result.resolve(r);
+				});
+			}
+		});
+
+		return result;
+	}
+
+	public static class VisitorResult<Parameter, Result> {
+		public Parameter NextIterationParameter;
+		public Result CurrentIterationResult;
+
+		public VisitorResult(Parameter nextIterationParameter, Result currentIterationResult) {
+			this.NextIterationParameter = nextIterationParameter;
+			this.CurrentIterationResult = currentIterationResult;
+		}
+	}
+}
